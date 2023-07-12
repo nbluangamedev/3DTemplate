f2018a7ca6243f13e3a0976a3745b88a4d108492
NAUL <nbluangamedev@gmail.com>
2023-07-12 21:51:29 +0700

    [fix] navigation bake

--- Assets/Scripts/CrossHairTarget.cs
+++ Assets/Scripts/CrossHairTarget.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrosshairTarget : MonoBehaviour
+public class CrossHairTarget : MonoBehaviour
 {
     public Camera mainCamera;
 
